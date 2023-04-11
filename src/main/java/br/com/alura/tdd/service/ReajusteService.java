02ee302c6338c48e169481f3de51a649c14c874c
Denise Anjos <deniseb.anjos@outlook.com>
2023-04-11 18:09:35 -0300

    Refatoracao da implementacao feita atraves de TDD

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -1,24 +1,14 @@
 package br.com.alura.tdd.service;
 
-import java.math.BigDecimal;
-
 import br.com.alura.tdd.modelo.DesempenhoeEnum;
 import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteService {
 	
 	public void calcularReajuste(Funcionario funcionario, DesempenhoeEnum desempenho) {
-		var reajuste = new BigDecimal("0");
-		if(desempenho.equals(DesempenhoeEnum.A_DESEJAR)) {
-			reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-			funcionario.reajustarSalario(reajuste);
-		} else if(desempenho.equals(DesempenhoeEnum.BOM)) {
-			reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-			funcionario.reajustarSalario(reajuste);
-		} else if (desempenho.equals(DesempenhoeEnum.OTIMO)) {
-			reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-			funcionario.reajustarSalario(reajuste);
-		}
+		var percentual = desempenho.percentualReajuste();
+		var reajuste = funcionario.getSalario().multiply(percentual);
+		funcionario.reajustarSalario(reajuste);
 	}
 
 }
